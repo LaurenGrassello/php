bffea078fffdb6325f1becdb834eea2b3bae5136
LaurenOdalen <laurenodalen@gmail.com>
2022-06-17 09:38:36 -0600

    functions

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+/*
+functions
+*/
+?>
+
+<?php
+function myFunction(){
+    echo "Hello World";
+}
+myFunction();
+?>
+
+<?php
+function anotherFunction($fname, $lname){
+    return $fname;
+};
+?>
