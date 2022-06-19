44a6e4ceafcd2b35906787d912c697c60a4cf6a7
LaurenOdalen <laurenodalen@gmail.com>
2022-06-19 13:32:08 -0600

    functions

--- functions.php
+++ functions.php
@@ -12,7 +12,24 @@ myFunction();
 ?>
 
 <?php
-function anotherFunction($fname, $lname){
-    return $fname;
-};
+function nameFunction($fname, $lname){
+    echo "$fname";
+}
+nameFunction("lauren", "odalen");
 ?>
+
+<?php
+function nameFunctionTwo($fname, $lname){
+    echo "$lname";
+}
+nameFunctionTwo("lauren", "odalen");
+?>
+
+
+<?php
+function fullName($fname, $lname){
+    echo "$fname", " $lname";
+}
+fullName("lauren", "odalen");
+?>
+
