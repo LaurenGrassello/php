c1db46e108f63ebc87745c0716dec2420186b815
LaurenOdalen <laurenodalen@gmail.com>
2022-06-21 13:27:50 -0600

    dates

---
+++ dates.php
@@ -0,0 +1,22 @@
+<?php
+/*Get day of the week 
+ex: Monday */
+?>
+<?php
+echo date("l");
+?>
+
+<?php
+/*Get date year.month.day*/
+?>
+<?php
+echo date("Y.m.d");
+?>
+
+<?php
+/*Get time using 24-hour format*/
+?>
+
+<?php
+echo date("H:i:sa");
+?>
