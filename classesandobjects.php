5cdf0bfb844b5a3ecc2b944ead76c321ef7c920b
LaurenOdalen <laurenodalen@gmail.com>
2022-06-22 08:25:41 -0600

    classes and objects

---
+++ classesandobjects.php
@@ -0,0 +1,37 @@
+<?php
+class Pet {
+    public $name;
+    public $age;
+
+    function set_name($name){
+        $this->name = $name;
+    }
+    function get_name(){
+        return $this->name;
+    }
+
+    function set_age($age){
+        $this->age = $age;
+    }
+    function get_age(){
+        return $this->age;
+    }
+}
+
+$dog = new Pet();
+$cat = new Pet();
+
+$dog->set_name('Marley ');
+$cat->set_name('Soma ');
+
+echo $dog->get_name();
+echo $cat->get_name();
+
+$dog->set_age('10 ');
+$cat->set_age('1 ');
+
+echo $dog->get_age();
+echo $cat->get_age();
+
+
+?>
